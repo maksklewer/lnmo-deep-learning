6b729434091db9d2c68dc2a1534816d44704226f
Kell <maybe.hello.world@gmail.com>
2018-10-09 19:38:50 +0300

    changed a bit added setUpClass np.matrix changed to np.ndarray (because matrix is deprecated)

--- Tests/test_task001.py
+++ Tests/test_task001.py
@@ -2,39 +2,37 @@ import unittest
 from unittest import TestCase
 from Tasks.Task001 import MatrixCalculator
 
-import numpy as np;
+import numpy as np
 
 
 class TestTask001(TestCase):
-    def test_sum_normal(self):
-        calculator = MatrixCalculator()
-        result = calculator.sum(np.matrix('1 1; 2 2'), np.matrix('1 1;2 2'))
-        np.testing.assert_array_equal(result, np.matrix('2 2;4 4'))
+    @classmethod
+    def setUpClass(cls):
+        cls.calculator = MatrixCalculator()
 
-    def test_sum_dif(self):
-        calculator = MatrixCalculator()
-        result = calculator.sum(np.matrix('1 1; 2 2'), np.matrix('1 1'))
-        np.testing.assert_array_equal(result, np.matrix('2 2;2 2'))
+    def test_sum_normal(self):
+        result = self.calculator.sum(np.array([[1, 1], [2, 2]]), np.array([[1, 1], [2, 2]]))
+        np.testing.assert_array_equal(result, np.array([[2, 2], [4, 4]]))
+
+    # def test_sum_dif(self):
+    #     result = self.calculator.sum(np.array([[1, 1], [2, 2]]), np.array([1, 1]))
+    #     np.testing.assert_array_equal(result, np.array([[2, 2], [2, 2]]))
 
     def test_sum_error(self):
-        calculator = MatrixCalculator()
-        result = calculator.sum(1, 2)
-        self.assertRaises(Exception)
+        with self.assertRaises(TypeError):
+            self.calculator.sum(1, 2)
 
     def test_mult_normal(self):
-        calculator = MatrixCalculator()
-        result = calculator.mult(np.matrix('1 1; 2 2'), np.matrix('1 1;2 2'))
-        np.testing.assert_array_equal(result, np.matrix('3 3;6 6'))
+        result = self.calculator.mult(np.array([[1, 1], [2, 2]]), np.array([[1, 1], [2, 2]]))
+        np.testing.assert_array_equal(result, np.array([[3, 3], [6, 6]]))
 
     def test_mult_dif(self):
-        calculator = MatrixCalculator()
-        result = calculator.mult(np.matrix('1 1; 2 2'), np.matrix('1 1'))
-        np.testing.assert_array_equal(result, np.matrix('2 2;4 4'))
+        result = self.calculator.mult(np.array([[1, 1], [2, 2]]), np.array([[1], [1]]))
+        np.testing.assert_array_equal(result, np.array([[2], [4]]))
 
     def test_mult_error(self):
-        calculator = MatrixCalculator()
-        result = calculator.mult(1, 1)
-        self.assertRaises(Exception)
+        with self.assertRaises(TypeError):
+            self.calculator.mult(1, 1)
 
 
 if __name__ == '__main__':
